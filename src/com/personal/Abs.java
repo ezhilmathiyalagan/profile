54e8fb27f65a774dd0c79dc38fc6ad46665a4d3e
ezhilmathiyalagan <ezhilmathiyalagan@gmail.com>
2018-06-05 15:18:48 +0530

    abstract

---
+++ src/com/personal/Abs.java
@@ -0,0 +1,9 @@
+package com.personal;
+
+public abstract class Abs {
+public Abs() {
+	
+}
+public abstract void getItem(); 
+
+}
---
+++ src/com/personal/SubAbs.java
@@ -0,0 +1,12 @@
+package com.personal;
+
+public class SubAbs extends Abs {
+public void getItem() {
+}{
+System.out.println("abstract class");
+}
+public static void main(String[]abs) {
+	SubAbs sb=new SubAbs();
+	sb.getItem();
+}
+}
